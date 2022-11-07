58269702639581091d12ff2a216a244a2e463303
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-11-07 23:48:23 +0100

    Update index.tsx

--- claim-app/pages/index.tsx
+++ claim-app/pages/index.tsx
@@ -1,5 +1,4 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { useProgram , useClaimNFT, useProrgamMetadata, useClaimConditions } from "@thirdweb-dev/react/solana"
 import type { NextPage } from "next";
 import Claim from "../components/Claim";
 
@@ -7,8 +6,6 @@ import Claim from "../components/Claim";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const Home: NextPage = () => {
-     const { program } = useProgram("3PHKaLH1QwP3jWbwBvn3PjhsW4b7cgpTPKbEKNwsgKJT", "nft-drop");
-     const { mutateAsync: claim, isLoading, error } = useClaimNFT(program);
     return(
       <div>
         <WalletMultiButton />
